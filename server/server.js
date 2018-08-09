7ade420c47e7b9cd1513df2bb26d82f94a808665
Jonathan Kruse <jonathanskruse@gmail.com>
2018-08-08 22:01:14 -0400

    fav

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.use(bodyParser.urlencoded({ extended: true }));
 
 
 app.use(express.static('public/static'));
-app.use(favicon(path.join(__dirname, 'favicon.ico')));
+app.use(favicon(path.join(__dirname, 'faviconn.ico')));
 app.use('/', contactForm);
 
 // App Set //
